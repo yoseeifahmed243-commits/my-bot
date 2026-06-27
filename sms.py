16d04f42623b865c94b2dde8f63c8463508b89b2
yoseeifahmed243-commits <yoseeifahmed243@gmail.com>
2026-06-27 11:19:32 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -5,22 +5,11 @@ def sms_menu():
     markup = InlineKeyboardMarkup(row_width=2)
 
     markup.add(
-        InlineKeyboardButton("📲 Telegram", callback_data="sms_telegram"),
+        InlineKeyboardButton("📲 Telegram", callback_data="sms_telegram")
+    )
+
+    markup.add(
         InlineKeyboardButton("🟢 WhatsApp", callback_data="sms_whatsapp")
-    )
-
-    markup.add(
-        InlineKeyboardButton("📸 Instagram", callback_data="sms_instagram"),
-        InlineKeyboardButton("🎵 TikTok", callback_data="sms_tiktok")
-    )
-
-    markup.add(
-        InlineKeyboardButton("🔵 Google", callback_data="sms_google"),
-        InlineKeyboardButton("🍎 Apple", callback_data="sms_apple")
-    )
-
-    markup.add(
-        InlineKeyboardButton("💙 PayPal", callback_data="sms_paypal")
     )
 
     markup.add(
